e5f8908ed467bce6cdef2992efa677c44359fd29
Max <max.haufe@htw-dresden.de>
2021-03-23 22:38:00 +0100

    added existing progress

---
+++ main.py
@@ -0,0 +1,51 @@
+import sys
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+from time import sleep
+
+
+def bump_trades(url, email, password):
+    path_to_chrome_driver = sys.argv[1]
+    path_to_adblock = sys.argv[2]
+
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument("--start-maximized")
+    # chrome_options.add_experimental_option("excludeSwitches", ["disable-popup-blocking"])
+    chrome_options.add_argument('load-extension=' + path_to_adblock)
+
+    driver = webdriver.Chrome(path_to_chrome_driver, options=chrome_options)
+    driver.get("http://www.google.com")
+    driver.get(url)
+
+    # accept privacy policy
+    WebDriverWait(driver, 20).until(ec.element_to_be_clickable((By.ID, 'acceptPrivacyPolicy'))).click()
+    # accept privacy policy again (wtf)
+    WebDriverWait(driver, 20).until(ec.element_to_be_clickable((By.CLASS_NAME, 'css-1tbbj19'))).click()
+    # input email
+    WebDriverWait(driver, 20).until(ec.element_to_be_clickable((By.ID, 'header-email'))).send_keys(email)
+    # input password
+    WebDriverWait(driver, 20).until(ec.element_to_be_clickable((By.ID, 'header-password'))).send_keys(password)
+    # GO
+    WebDriverWait(driver, 20).until(ec.element_to_be_clickable((By.NAME, 'submit'))).click()
+    # todo exception handling
+    # no button exists, trade was already bömped
+    WebDriverWait(driver, 20).until(
+        ec.element_to_be_clickable((By.CLASS_NAME, 'rlg-trade__action.rlg-trade__bump.--bump')))
+    bump_button_list = driver.find_elements_by_class_name('rlg-trade__action.rlg-trade__bump.--bump')
+    for bump_button in bump_button_list:
+        bump_button.click()
+        sleep(2)  # idk if that is necessary
+        WebDriverWait(driver, 20).until(ec.element_to_be_clickable((By.XPATH, '/html/body/div[2]/div/span/i'))).click()
+
+
+email = sys.argv[3]
+password = sys.argv[4]
+url = sys.argv[5]
+
+bump_trades(url, email, password)
