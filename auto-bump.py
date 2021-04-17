c9f9d7a0bea6f6d5d7442682484ce774735820e3
max <max.haufe@htw-dresden.de>
2021-04-17 17:32:10 +0200

    refractor

--- auto-bump.py
+++ auto-bump.py
@@ -9,16 +9,14 @@ from selenium.webdriver.chrome.options import Options
 from time import sleep
 
 
-def bump_trades(url, email, password, path_to_adblock, path_to_chrome_driver):
+def bump_trades(url, email, password, path_to_chrome_driver):
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--window-size=1920,1080")
     chrome_options.add_argument("--start-maximized")
     chrome_options.add_experimental_option("excludeSwitches", ["disable-popup-blocking"])
-    # chrome_options.add_argument('load-extension=' + path_to_adblock)
 
     driver = webdriver.Chrome(path_to_chrome_driver, options=chrome_options)
-    # driver.get("http://www.google.com")
     driver.get(url)
 
     # accept privacy policy
@@ -49,11 +47,9 @@ config.read(path_to_config)
 # todo error handling
 email = config["DEFAULT"]["email"]
 password = config["DEFAULT"]["password"]
-# path_to_adblock=config["DEFAULT"]["path_to_adblock"]
-path_to_adblock = ""
 path_to_chrome_driver = config["DEFAULT"]["path_to_chrome_driver"]
 username = config["DEFAULT"]["username"]
 
 url = "https://rocket-league.com/trades/" + username
 
-bump_trades(url, email, password, path_to_adblock, path_to_chrome_driver)
+bump_trades(url, email, password, path_to_chrome_driver)
--- auto-respond.py
+++ auto-respond.py
@@ -1,6 +1,5 @@
-import sys
-import argparse
 import configparser
+import os
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -18,7 +17,7 @@ def bump_trades(url, email, password, path_to_adblock, path_to_chrome_driver):
     chrome_options.add_argument("--window-size=1920,1080")
     chrome_options.add_argument("--start-maximized")
     chrome_options.add_experimental_option("excludeSwitches", ["disable-popup-blocking"])
-    chrome_options.add_argument('load-extension=' + path_to_adblock)
+    # chrome_options.add_argument('load-extension=' + path_to_adblock)
 
     driver = webdriver.Chrome(path_to_chrome_driver, options=chrome_options)
     # driver.get("http://www.google.com")
@@ -37,27 +36,32 @@ def bump_trades(url, email, password, path_to_adblock, path_to_chrome_driver):
     # click on speechbubble if new notifictation, if not time out
     WebDriverWait(driver, 20).until(
         ec.element_to_be_clickable((By.CLASS_NAME, 'rlg-message-notification.--new'))).click()
+    # WebDriverWait(driver, 20).until(
+    #     ec.element_to_be_clickable((By.CLASS_NAME, 'rlg-message-notification'))).click()
     print("new messages found")
     # for each "new" chat do...
-    new_chat_list = driver.find_elements_by_class_name('rlg-chat__thread.--is-user.--new.--not-archived')
+    new_chat_list = driver.find_elements_by_class_name('rlg-chat__thread.--new.--active')
+    # new_chat = driver.find_element_by_class_name('rlg-chat__thread.--active')
 
     for new_chat in new_chat_list:
+    # if True:
         new_chat.click()
         sleep(2)
         WebDriverWait(driver, 20).until(
-            ec.element_to_be_clickable((By.CLASS_NAME, 'rlg-input.rlg-chat__input'))).send_keys(message)
+            ec.element_to_be_clickable((By.ID, 'messagetext'))).send_keys(message)
         sleep(5)
         WebDriverWait(driver, 20).until(
             ec.element_to_be_clickable((By.CLASS_NAME, 'fa.fa-paper-plane-o'))).click()
 
 
 config = configparser.ConfigParser()
-config.read("rl_garage.ini")
+path_to_config = os.path.dirname(os.path.abspath(__file__)) + "/rl_garage.ini"
+config.read(path_to_config)
 
 # todo error handling
 email = config["DEFAULT"]["email"]
 password = config["DEFAULT"]["password"]
-path_to_adblock = config["DEFAULT"]["path_to_adblock"]
+path_to_adblock = "" # config["DEFAULT"]["path_to_adblock"]
 path_to_chrome_driver = config["DEFAULT"]["path_to_chrome_driver"]
 username = config["DEFAULT"]["username"]
 
